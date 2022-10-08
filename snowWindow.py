2681c5ec7d692240015e95e552e5c34d9361af1d
Paul Ambrose <pablo_rose34@hotmail.com>
2022-10-08 13:48:07 +0100

    Create snowWindow.py

---
+++ snowWindow.py
@@ -0,0 +1,9 @@
+#Create a small window where it appears to snow from the top of the screen.  The snow flakes descending the screen and settling at the bottom.
+#The program ends when the window is full
+
+#Steps
+#Create a window
+#Create a snowflakeclass, which creates a random flake (white pixel) which descdends the screen and stops at the bottom
+  #Redefine the bottom of the screen as where the pixel is
+#Implement a number of snowFlake objects
+

fcdb3bc858b0221c122be2dbafef7d2098ccc25d
Paul Ambrose <pablo_rose34@hotmail.com>
2022-10-10 20:09:24 +0100

    update

--- snowWindow/files/snowFlakeClass.py
+++ snowWindow/files/snowFlakeClass.py
@@ -1,39 +1,39 @@
 import pygame
 from pygame.locals import *
-import sys
+import random
 
-BLACK = (0, 0, 0)
-WHITE = (255, 255, 255)
-WINDOW_WIDTH = 640
-WINDOW_HEIGHT = 480
-FRAMES_PER_SECOND = 30
-N_FLAKES = 300
 
-pygame.init()
-window = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
-clock = pygame.time.Clock()
+class flake():
 
-flakeList = []
-for oFlake in range(0, N_FLAKES):
-    oFlake = pygame.draw.circle(window, WHITE, (250, 50), 1, 0)
-    flakeList.append(oFlake)
+    def __init__(self, window, windowWidth, windowHeight):
+        self.window = window
+        self.windowWidth = windowWidth
+        self.windowHeight = windowHeight
+        self.WHITE = (0, 0, 0)
 
-while True:
+        self.image = pygame.draw.circle(window, self.WHITE, (250, 50), 1, 0)
+        flakeRect = self.image.get_rect()
+        self.width = flakeRect.width
+        self.height = flakeRect.height
+        self.maxWidth = windowWidth - self.width
+        self.maxHeight = windowHeight - self.height
 
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            sys.exit()
+        self.x = random.randrange(0, self.maxWidth)
+        self.y = 0
 
-    for oFlake in flakeList:
-        oFlake.update()
-    window.fill(BLACK)
+        speedsList = [-4, -3, -2, -1, 1, 2, 3, 4]
+        self.xSpeed = random.choice(speedsList)
+        self.ySpeed = random.choice(speedsList)
 
-    for oFlake in flakeList:
-        oFlake.draw()   # tell each Ball to draw itself
+    def update(self):
+        if (self.x < 0) or (self.x >= self.maxWidth):
+            self.xSpeed = -self.xSpeed
 
-    # 11 - Update the window
-    pygame.display.update()
+        if (self.y < 0) or (self.y >= self.maxHeight):
+            self.ySpeed = -self.ySpeed
 
-    # 12 - Slow things down a bit
-    clock.tick(FRAMES_PER_SECOND)  # make pygame wait
+        self.x = self.x + self.xSpeed
+        self.y = self.y + self.ySpeed
+
+    def draw(self):
+        self.window.blit(self.image, (self.x, self.y))
